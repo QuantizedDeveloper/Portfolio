2fa35af149520160b7bee18c88c762dd3f47b9c5
QuantizedDeveloper <quantizeddeveloper@gmail.com>
2026-07-19 08:43:42 +0530

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import {HashRouter} from "react-router-dom";
 import App from './App.jsx'
 import "./styles.css";
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
      
 
    <App />
-  </BrowserRouter>
+  </StrictMode>
 )
